e4b575b93aaf518c430f7fbbeec006ba403afe2f
William Safra <61418031+Safra-Man@users.noreply.github.com>
2020-12-04 15:46:11 -0300

    Update main.c

--- C_00/main.c
+++ C_00/main.c
@@ -42,3 +42,30 @@ int	main(void)
 	ft_putchar('\n');
 	return (0);
 }
+/*
+alias www='gcc -Wall -Wextra -Werror *.c ; ./a.out 'OK!!'
+
+// Função para Compilar usando o comando www al invés do textão ai
+
+
+alias mobilete="norminette -R CheckForbiddenSourceHeader" &&
+alias ce="gcc -Wall -Wextra -Werror *.c" &&
+alias ec="./a.out" &&
+alias 00="cd ex00" &&
+alias g0="cd ../ex00" &&
+alias g1="cd ../ex01" &&
+alias g2="cd ../ex02" &&
+alias g3="cd ../ex03" &&
+alias g4="cd ../ex04" &&
+alias g5="cd ../ex05" &&
+alias g6="cd ../ex06" &&
+alias g7="cd ../ex07" &&
+alias g8="cd ../ex08" &&
+alias g9="cd ../ex09" &&
+alias g10="cd ../ex10" &&
+alias g11="cd ../ex11" &&
+alias g12="cd ../ex12"
+
+
+gcc -Wall -Wextra -Werror ./main.c ex0{0..7}/*.c -o teste && ./teste && rm teste
+*/
