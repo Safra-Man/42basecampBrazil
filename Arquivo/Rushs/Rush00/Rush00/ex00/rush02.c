a18596e3efff71ae6e788148c8ab50fc32faa2d8
wsafra-l <wsafra-l@student.42sp.org.br>
2020-12-04 20:26:25 +0000

    Arquivo

---
+++ Arquivo/Rushs/Rush00/Rush00/ex00/rush02.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush02.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gunoguei <gunoguei@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/22 03:27:47 by danascim          #+#    #+#             */
+/*   Updated: 2020/11/23 00:02:54 by gunoguei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	coluna(int x, char a, char b, char c)
+{
+	if (x > 0)
+	{
+		ft_putchar(a);
+		x--;
+	}
+	while (x > 1)
+	{
+		ft_putchar(b);
+		x--;
+	}
+	if (x == 1)
+	{
+		ft_putchar(c);
+	}
+	ft_putchar('\n');
+}
+
+void	rush(int x, int y)
+{
+	if (x > 0)
+	{
+		if (y > 0)
+		{
+			coluna(x, 'A', 'B', 'A');
+			y--;
+		}
+		while (y > 1)
+		{
+			coluna(x, 'B', ' ', 'B');
+			y--;
+		}
+		if (y == 1)
+		{
+			coluna(x, 'C', 'B', 'C');
+		}
+	}
+}
---
+++ Arquivo/Shell001/ex04/git_commit.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+git log -n5 | grep commit | cut -c 8-47 
